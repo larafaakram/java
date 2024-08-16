bb7244e6275f44e84d4dc388113b02135ed53e7c
larafaakram <larafa.akram@gmail.com>
2024-08-16 17:04:31 +0100

    Execute all pipeline stage

--- src/main/java/com/valaxy/demo/controller/RepositoryDetailsController.java
+++ src/main/java/com/valaxy/demo/controller/RepositoryDetailsController.java
@@ -34,7 +34,7 @@ public class RepositoryDetailsController {
 	public String getRepos() throws IOException {
 		GitHub github = new GitHubBuilder().withPassword("valaxytech@gmail.com", "XXXXXXXX").build();
 		GHRepositorySearchBuilder builder = github.searchRepositories();
-		return "Greetings from NamG DevOps Talks";
+		return "Greetings from NamG DevOps Talks, Version 35";
 	}
 
 	@GetMapping("/trends")
